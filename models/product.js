9ee691749cecbc8dce091e26723788a3ae31972d
karthik-cj <karthikrampo666@gmail.com>
2022-07-05 15:11:48 +0530

    Making Changes

--- models/product.js
+++ models/product.js
@@ -29,6 +29,10 @@ const productSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  shop: {
+    type: String,
+    required: true,
+  },
 
 });
 
--- routes/product.js
+++ routes/product.js
@@ -3,9 +3,9 @@ const productRouter = express.Router();
 const auth = require("../middlewares/auth");
 const { Product } = require("../models/product");
 
-productRouter.get("/products/", auth, async (req, res) => {
+productRouter.get("/productsinshop", auth, async (req, res) => {
   try {
-    const products = await Product.find({ category: req.query.category });
+    const products = await Product.find({ shop: req.query.shop });
     res.json(products);
   } catch (e) {
     res.status(500).json({ error: e.message });
@@ -23,30 +23,5 @@ productRouter.get("/products/search/:name", auth, async (req, res) => {
     res.status(500).json({ error: e.message });
   }
 });
-
-
-// productRouter.get("/", auth, async (req, res) => {
-//   try {
-//     let products = await Product.find({});
-
-//     products = products.sort((a, b) => {
-//       let aSum = 0;
-//       let bSum = 0;
-
-//       for (let i = 0; i < a.ratings.length; i++) {
-//         aSum += a.ratings[i].rating;
-//       }
-
-//       for (let i = 0; i < b.ratings.length; i++) {
-//         bSum += b.ratings[i].rating;
-//       }
-//       return aSum < bSum ? 1 : -1;
-//     });
-
-//     res.json(products[0]);
-//   } catch (e) {
-//     res.status(500).json({ error: e.message });
-//   }
-// });
 
 module.exports = productRouter;
