9862e90d0d33845778326b45fdd84a61e337fb94
karthik-cj <karthikrampo666@gmail.com>
2022-07-23 03:55:56 +0530

    make it better

--- routes/product.js
+++ routes/product.js
@@ -3,14 +3,22 @@ const productRouter = express.Router();
 const auth = require("../middlewares/auth");
 const { Product } = require("../models/product");
 const Shop = require("../models/shop");
+const User = require("../models/user");
 
 productRouter.get("/products/:shop", auth, async (req, res) => {
   try {
+    let fav = false;
+    let shop = await Shop.findById(req.params.shop);
+    let user = await User.findById(req.user);
+
+    for (let i = 0; i < user.favourites.length; i++) {
+      if (user.favourites[i].name == shop.name) fav = true;
+    }
+
     const products = await Product.find({
       shop: { $elemMatch: { $eq: req.params.shop } },
     });
-
-    res.status(200).json(products);
+    res.status(200).json({ products, fav: fav });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
